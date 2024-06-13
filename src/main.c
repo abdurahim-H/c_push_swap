0e68ec38de5b1739a77d369f3f09df5b852dcfc4
Abdurahim Hudulov <136490387+abdurahim-H@users.noreply.github.com>
2024-06-13 14:10:34 +0200

    better memory management

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abhudulo <abhudulo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 22:43:47 by event             #+#    #+#             */
-/*   Updated: 2024/06/13 13:04:46 by abhudulo         ###   ########.fr       */
+/*   Updated: 2024/06/13 14:07:51 by abhudulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,53 +74,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
-// int main(int argc, char *argv[]) {
-//     if (argc <= 1) {
-//         return 0;
-//     }
-
-//     t_stack a, b;
-//     t_command *cmd_list = NULL;
-//     int capacity = argc - 1;
-
-//     init_stack(&a, capacity);
-//     init_stack(&b, capacity);
-
-//     if (process_args(argc, argv, &a)) {
-//         free_stack(&a);
-//         free_stack(&b);
-//         return 1;
-//     }
-
-// 	if (check_duplicates(&a)) {
-//         printf("Error: Duplicate values are not allowed.\n");
-//         free_stack(&a);
-//         free_stack(&b);
-//         return 1;
-//     }
-
-//     if (is_reverse_sorted(&a)) {
-//         free_stack(&a);
-//         free_stack(&b);
-//         return 0;
-//     }
-
-//     sort_stack(&a, &b, &cmd_list);
-//     print_commands(cmd_list);
-
-//     // Print the final sorted stack
-//     t_node *node = a.top;
-//     printf("Sorted stack: ");
-//     while (node) {
-//         printf("%d ", node->value);
-//         node = node->next;
-//     }
-//     printf("\n");
-
-//     free_commands(cmd_list);
-//     free_stack(&a);
-//     free_stack(&b);
-
-//     return 0;
-// }
